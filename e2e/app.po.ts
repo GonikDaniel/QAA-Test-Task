01653c776fdd60039739d2017dffc70154023c9e
Kozub Nikolay Nikolaevich <n.kozub@tcsbank.ru>
2018-01-24 13:55:03 +0300

    new tests +steps.docx

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,16 +1,44 @@
 import { browser, by, element } from 'protractor';
 import { byAutomationId } from './helpers';
 
-export class AppPage {
-  get formTitle() {
-    return byAutomationId('form-title');
-  }
 
-  navigateTo() {
+export class AppPage {
+    navigateTo() {
     return browser.get('/');
   }
 
-  getParagraphText() {
-    return this.formTitle.getText();
+
+  getTextbyId(value){
+    return byAutomationId(value).getText();
   }
-}
+
+   getInputCheckboxText() {
+    let Text = element(by.id('checkbox-text'));
+    return Text.getText();
+  }
+
+  getInputSubmit() {
+    return byAutomationId('input-submit').getAttribute('value');
+  }
+
+  getElementBySucces(){
+   return element(by.className('subheader'));
+  }
+
+  setTextById(id,value){
+    return byAutomationId(id).sendKeys(value);
+  }
+
+     ElementClick(id) {
+    return byAutomationId(id).click();
+  }
+
+  getElementByAutomationId(id){
+    return byAutomationId(id);
+  } 
+
+  getEelmentById(value){
+     return byAutomationId(value);
+  }
+ }
+
